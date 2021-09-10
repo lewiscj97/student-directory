21f11fd5e32a9647de44039446524f4a2687fb5c
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-10 11:43:56 +0100

    Get students name via user input

--- directory.rb
+++ directory.rb
@@ -1,8 +1,31 @@
-students = [
-  {name: "Hannibal Lecter", cohort: :November},
-  {name: "Darth Vadar", cohort: :November},
-  {name: "Terminator", cohort: :November}
-]
+def input_students
+  puts "Enter the name of the students"
+  puts "Press enter twice to exit"
+
+  # Create empty array
+  students = []
+
+  # Get input from user
+  input = gets.chomp
+  
+  # Check if input is empty - if it is, return the array
+  while !input.empty? do
+    # Add student to array
+    students << {name: input, cohort: :November}
+
+    # Print the number of students
+    if students.count == 1
+      puts "Now we have #{students.count} student"
+    else
+      puts "Now we have #{students.count} students"
+    end
+    
+    # Get a new input
+    input = gets.chomp
+  end
+
+  return students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -16,10 +39,15 @@ def print(students)
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students!"
+  if students.count == 1
+    puts "Overall, we have #{students.count} great student!"
+  else
+    puts "Overall, we have #{students.count} great students!"
+  end
 end
 
 # Call methods to run program
+students = input_students()
 print_header
 print(students)
 print_footer(students)
