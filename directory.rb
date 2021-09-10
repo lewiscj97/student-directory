c416d7226fe59e1c3dd08ad72122965ba1ad1ae4
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-10 11:01:24 +0100

    Print student list

---
+++ directory.rb
@@ -0,0 +1,11 @@
+# Print list of students
+puts "The students of Villains Academy"
+puts "-------------------"
+puts "Hannibal Lecter"
+puts "Darth Vadar"
+puts "Terminator"
+
+# Print total number of students
+print "Overall, we have "
+print 3
+puts " great students."
